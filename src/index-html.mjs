3173ef819897dbaad37a00d65ec02d42821a28a1
dqh-au <david.q.hogan@gmail.com>
2023-05-16 22:26:34 +1000

    Added h1 & h2 bottom border

--- src/index-html.mjs
+++ src/index-html.mjs
@@ -54,7 +54,7 @@ Page.AppendHead(
 Page.AppendBody(
     <>
         <main>
-            <h1 css="& svg { display: inline }"><raw-content content={logo} /> Documentation</h1>
+            <h1><raw-content content={logo} /> Documentation</h1>
             <p>NakedJSX builds static HTML files from JavaScript and JSX.</p>
             <p>It also supports:</p>
             <ul>
